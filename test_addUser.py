d9444dc022212a392d77765dd3145875cb50a85d
JIN <qianhui.jin@yahoo.fr>
2022-01-10 15:12:04 +0100

    🔨 add the test to check if the IP address and port are correct

--- test_addUser.py
+++ test_addUser.py
@@ -11,6 +11,16 @@ class TestUserSrv(unittest.TestCase):
     def test_checkUsername(self):
         self.assertFalse(db.checkUsername("")) # username empty 
         self.assertTrue(db.checkUsername("log")  
+    
+    def test_checkIP(self):
+        self.assertFalse(db.checkIP("")) # empty
+        self.assertFalse(db.checkIP("1")) # bad IP 
+        self.assertTrue(db.checkIP("123.283.288.283") 
+
+    def test_checkPort(self):
+        self.assertFalse(db.checkPort("")) # empty
+        self.assertFalse(db.checkPort(80000)) # bad port
+        self.assertTrue(db.checkPort(8080) 
 
     def test_addUser(self):
         self.assertFalse(db.addUser(self.test_db,"","0.0.0.0",80)) # username empty 
