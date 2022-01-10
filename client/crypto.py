34eeae4a159933d40f9b49c4efa54fc1703b3c2b
polopaul77 <paul.fourcade1@gmail.com>
2022-01-10 17:29:47 +0100

    :art: Reformat code and improve lisibility

--- client/crypto.py
+++ client/crypto.py
@@ -4,73 +4,73 @@ from Crypto.Cipher import PKCS1_OAEP
 from Crypto.PublicKey import RSA
 
 def generate_keys(key_size):
-	"""
-	Genére une paire de clé privée / publique
-	de la taille key_size d'au minimum 1024 bits
-	Retourne les deux clés au format string sous 
-	la forme d'une liste de deux éléments
-	"""
-	if key_size < 1024:
-		return None
-	else:
-		random_gen = Random.new().read
+    """
+    Genére une paire de clé privée / publique
+    de la taille key_size d'au minimum 1024 bits
+    Retourne les deux clés au format string sous
+    la forme d'une liste de deux éléments
+    """
+    if key_size < 1024:
+        return None
 
-		# Génération de la paire de clé de taille key_size
-		private_key = RSA.generate(key_size, random_gen)
-		public_key  = private_key.publickey()
+    random_gen = Random.new().read
 
-		# Conversion des clés au format texte (type string)
-		private_key_str = private_key.exportKey().decode()
-		public_key_str  = public_key.exportKey().decode()
+    # Génération de la paire de clé de taille key_size
+    private_key = RSA.generate(key_size, random_gen)
+    public_key = private_key.publickey()
 
-		return private_key_str, public_key_str
+    # Conversion des clés au format texte (type string)
+    private_key_str = private_key.exportKey().decode()
+    public_key_str = public_key.exportKey().decode()
+
+    return private_key_str, public_key_str
 
 def encrypt_message(public_key, message):
-	"""
-	Chiffrement d'un message à partir d'une clé publique
-	Retourne le message crypté au format string
-	"""
-	if message == "":
-		return None
-	else:
-		# Conversion de la clé publique de string vers RSA_key_format
-		# lorsque celle-ci est valide
-		try:
-			key = RSA.importKey(public_key)
-		except ValueError:
-			return None
+    """
+    Chiffrement d'un message à partir d'une clé publique
+    Retourne le message crypté au format string
+    """
+    if message == "":
+        return None
 
-		encryptor = PKCS1_OAEP.new(key)
-		msg_crypt = base64.b64encode(encryptor.encrypt(message.encode()))
-		
-		# Conversion au format texte (type string)
-		msg_crypt_str = msg_crypt.decode()
-		
-		return msg_crypt_str
+    # Conversion de la clé publique de string vers RSA_key_format
+    # lorsque celle-ci est valide
+    try:
+        key = RSA.importKey(public_key)
+    except ValueError:
+        return None
+
+    encryptor = PKCS1_OAEP.new(key)
+    msg_crypt = base64.b64encode(encryptor.encrypt(message.encode()))
+
+    # Conversion au format texte (type string)
+    msg_crypt_str = msg_crypt.decode()
+
+    return msg_crypt_str
 
 def decrypt_message(private_key, crypt_message):
-	"""
-	Déchiffrement d'un message à partir d'une clé privée
-	Retourne le message décrypté au format string
-	"""
-	if crypt_message == "":
-		return None
-	else:
-		# Conversion de la clé publique de string vers RSA_key_format
-		# lorsque celle-ci est valide
-		try:
-			key = RSA.importKey(private_key)
-		except ValueError:
-			return None
+    """
+    Déchiffrement d'un message à partir d'une clé privée
+    Retourne le message décrypté au format string
+    """
+    if crypt_message == "":
+        return None
 
-		decryptor = PKCS1_OAEP.new(key)
+    # Conversion de la clé publique de string vers RSA_key_format
+    # lorsque celle-ci est valide
+    try:
+        key = RSA.importKey(private_key)
+    except ValueError:
+        return None
 
-		try:
-			message = decryptor.decrypt(base64.b64decode(crypt_message.encode()))
-		except ValueError:
-			return None
+    decryptor = PKCS1_OAEP.new(key)
 
-		# Conversion au format texte (type string)
-		message_str = message.decode()
-		
-		return message_str
+    try:
+        message = decryptor.decrypt(base64.b64decode(crypt_message.encode()))
+    except ValueError:
+        return None
+
+    # Conversion au format texte (type string)
+    message_str = message.decode()
+
+    return message_str
--- tests/test_crypto.py
+++ tests/test_crypto.py
@@ -1,81 +1,87 @@
 import unittest
 from Crypto.PublicKey import RSA
-import sys
 
-sys.path.append("../client")
-from crypto import *
+from client.crypto import generate_keys, encrypt_message, decrypt_message
 
 class TestCrypto(unittest.TestCase):
-	
-	# Tests sur la génération des clés privées et publiques
-	
-	def test_minimum_key_size(self):
-		# Taille minium de 1024 bits
-		self.assertEqual(generate_keys(512), None)
-		self.assertEqual(generate_keys(0), None)
-		self.assertEqual(generate_keys(1023), None)
-		self.assertIsInstance(generate_keys(1024)[0], str)
-		self.assertIsInstance(generate_keys(1024)[1], str)
-		self.assertIsInstance(generate_keys(4096)[0], str)
 
-	def test_key_size(self):
-		# Vérification de la taille des clés
-		keys_1 = generate_keys(1024)
-		self.assertEqual(RSA.importKey(keys_1[0]).n.bit_length(), 1024)
-		self.assertEqual(RSA.importKey(keys_1[1]).n.bit_length(), 1024)
-		keys_2 = generate_keys(4096)
-		self.assertEqual(RSA.importKey(keys_2[0]).n.bit_length(), 4096)
-		self.assertEqual(RSA.importKey(keys_2[1]).n.bit_length(), 4096)
+    # Tests sur la génération des clés privées et publiques
 
-	def test_key_unique(self):
-		# Unicité des clés privées et publiques
-		self.assertNotEqual(generate_keys(2048)[0], generate_keys(2048)[0])
-		self.assertNotEqual(generate_keys(1024)[0], generate_keys(1024)[0])
-		self.assertNotEqual(generate_keys(2048)[1], generate_keys(2048)[1])
-		keys = generate_keys(2048)
-		self.assertNotEqual(keys[0], keys[1])
-	
-	# Tests sur le chiffrement d'un message
+    def test_minimum_key_size(self):
+        # Taille minium de 1024 bits
+        self.assertEqual(generate_keys(512), None)
+        self.assertEqual(generate_keys(0), None)
+        self.assertEqual(generate_keys(1023), None)
+        self.assertIsInstance(generate_keys(1024)[0], str)
+        self.assertIsInstance(generate_keys(1024)[1], str)
+        self.assertIsInstance(generate_keys(4096)[0], str)
 
-	def test_encryption_message(self):
-		# Vérification du format du message à chiffrer
-		keys = generate_keys(2048)
-		self.assertEqual(encrypt_message(keys[1], ""), None)
-		self.assertIsInstance(encrypt_message(keys[1], "A"), str)
-		self.assertIsInstance(encrypt_message(keys[1], "abcdefghijklmnopqrstuvwxyzAZERTYUIOP\n1234567890 &éçàèùïüö\t,?;.:/!§%*µ£=+})°]@"), str)
-	
-	def test_encryption_key(self):
-		# Vérification de la clé
-		self.assertEqual(encrypt_message("Fake key:CBVscPeuYkXW4/jjhinp", "message"), None)
-		self.assertIsInstance(encrypt_message(generate_keys(2048)[1], "message"), str)
+    def test_key_size(self):
+        # Vérification de la taille des clés
+        keys_1 = generate_keys(1024)
+        self.assertEqual(RSA.importKey(keys_1[0]).n.bit_length(), 1024)
+        self.assertEqual(RSA.importKey(keys_1[1]).n.bit_length(), 1024)
+        keys_2 = generate_keys(4096)
+        self.assertEqual(RSA.importKey(keys_2[0]).n.bit_length(), 4096)
+        self.assertEqual(RSA.importKey(keys_2[1]).n.bit_length(), 4096)
 
-	def test_encryption_content(self):
-		# Vérification du chiffrement (différent du message d'origine)
-		self.assertNotEqual(encrypt_message(generate_keys(1024)[1], "a"), "a")
-		self.assertNotEqual(encrypt_message(generate_keys(2048)[1], "message"), "message")
-		self.assertNotEqual(encrypt_message(generate_keys(2048)[1], "abcdefghijklmnopqrstuvwxyzAZERTYUIOP\n1234567890 &éçàèùïüö\t,?;.:/!§%*µ£=+})°]@"), "abcdefghijklmnopqrstuvwxyzAZERTYUIOP\n1234567890 &éçàèùïüö\t,?;.:/!§%*µ£=+})°]@")
-	
-	# Tests sur le déchiffrement d'un message
+    def test_key_unique(self):
+        # Unicité des clés privées et publiques
+        self.assertNotEqual(generate_keys(2048)[0], generate_keys(2048)[0])
+        self.assertNotEqual(generate_keys(1024)[0], generate_keys(1024)[0])
+        self.assertNotEqual(generate_keys(2048)[1], generate_keys(2048)[1])
+        keys = generate_keys(2048)
+        self.assertNotEqual(keys[0], keys[1])
 
-	def test_decryption_message(self):
-		# Vérification du format du message à déchiffrer
-		keys = generate_keys(2048)
-		self.assertEqual(decrypt_message(keys[0], ""), None)
-		self.assertEqual(decrypt_message(keys[0], "Fake_crypt_msg:CBVscPeuYkXW4/jjhinp"), None)
-		self.assertIsInstance(decrypt_message(keys[0], encrypt_message(keys[1], "A")), str)
+    # Tests sur le chiffrement d'un message
 
-	def test_decryption_key(self):
-		# Vérification de la clé
-		keys = generate_keys(2048)
-		self.assertEqual(decrypt_message("Fake key:CBVscPeuYkXW4/jjhinp", encrypt_message(keys[1], "A")), None)
-		self.assertIsInstance(decrypt_message(keys[0], encrypt_message(keys[1], "A")), str)
+    def test_encryption_message(self):
+        # Vérification du format du message à chiffrer
+        keys = generate_keys(2048)
+        self.assertEqual(encrypt_message(keys[1], ""), None)
+        self.assertIsInstance(encrypt_message(keys[1], "A"), str)
+        self.assertIsInstance(encrypt_message(keys[1], \
+        "abcdefghijklmnopqrstuvwxyzAZERTYUIOP\n1234567890 &éçàèùïüö\t,?;.:/!§%*µ£=+})°]@"), str)
 
-	def test_decryption_content(self):
-		# Vérification du déchiffrement
-		keys = generate_keys(2048)
-		self.assertEqual(decrypt_message(keys[0], encrypt_message(keys[1], "a")), "a")
-		self.assertEqual(decrypt_message(keys[0], encrypt_message(keys[1], "Hello world !")), "Hello world !")
-		self.assertEqual(decrypt_message(keys[0], encrypt_message(keys[1], "abcdefghijklmnopqrstuvwxyzAZERTYUIOP\n1234567890 &éçàèùïüö\t,?;.:/!§%*µ£=+})°]@")), "abcdefghijklmnopqrstuvwxyzAZERTYUIOP\n1234567890 &éçàèùïüö\t,?;.:/!§%*µ£=+})°]@")
+    def test_encryption_key(self):
+        # Vérification de la clé
+        self.assertEqual(encrypt_message("Fake key:CBVscPeuYkXW4/jjhinp", "message"), None)
+        self.assertIsInstance(encrypt_message(generate_keys(2048)[1], "message"), str)
+
+    def test_encryption_content(self):
+        # Vérification du chiffrement (différent du message d'origine)
+        self.assertNotEqual(encrypt_message(generate_keys(1024)[1], "a"), "a")
+        self.assertNotEqual(encrypt_message(generate_keys(2048)[1], "message"), "message")
+        self.assertNotEqual(encrypt_message(generate_keys(2048)[1], \
+        "abcdefghijklmnopqrstuvwxyzAZERTYUIOP\n1234567890 &éçàèùïüö\t,?;.:/!§%*µ£=+})°]@"), \
+        "abcdefghijklmnopqrstuvwxyzAZERTYUIOP\n1234567890 &éçàèùïüö\t,?;.:/!§%*µ£=+})°]@")
+
+    # Tests sur le déchiffrement d'un message
+
+    def test_decryption_message(self):
+        # Vérification du format du message à déchiffrer
+        keys = generate_keys(2048)
+        self.assertEqual(decrypt_message(keys[0], ""), None)
+        self.assertEqual(decrypt_message(keys[0], "Fake_crypt_msg:CBVscPeuYkXW4/jjhinp"), None)
+        self.assertIsInstance(decrypt_message(keys[0], encrypt_message(keys[1], "A")), str)
+
+    def test_decryption_key(self):
+        # Vérification de la clé
+        keys = generate_keys(2048)
+        self.assertEqual(decrypt_message("Fake key:CBVscPeuYkXW4/jjhinp", \
+        encrypt_message(keys[1], "A")), None)
+        self.assertIsInstance(decrypt_message(keys[0], encrypt_message(\
+        keys[1], "A")), str)
+
+    def test_decryption_content(self):
+        # Vérification du déchiffrement
+        keys = generate_keys(2048)
+        self.assertEqual(decrypt_message(keys[0], encrypt_message(keys[1], "a")), "a")
+        self.assertEqual(decrypt_message(keys[0], encrypt_message(keys[1], \
+        "Hello world !")), "Hello world !")
+        self.assertEqual(decrypt_message(keys[0], encrypt_message(keys[1], \
+        "abcdefghijklmnopqrstuvwxyzAZERTYUIOP\n1234567890 &éçàèùïüö\t,?;.:/!§%*µ£=+})°]@")), \
+        "abcdefghijklmnopqrstuvwxyzAZERTYUIOP\n1234567890 &éçàèùïüö\t,?;.:/!§%*µ£=+})°]@")
 
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
