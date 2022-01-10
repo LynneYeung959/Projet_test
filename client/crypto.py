e1618a45194a89b2fc3a2d7c3d72a6772fa2b52a
polopaul77 <paul.fourcade1@gmail.com>
2022-01-10 15:43:44 +0100

    :white_check_mark: Ajout de la fonction generate_keys et validation du test

--- client/crypto.py
+++ client/crypto.py
@@ -4,4 +4,23 @@ from Crypto.Cipher import PKCS1_OAEP
 from Crypto.PublicKey import RSA
 
 def generate_keys(key_size):
-	pass
+	"""
+	Genére une paire de clé privée / publique
+	de la taille key_size d'au minimum 1024 bits
+	Retourne les deux clés au format string sous 
+	la forme d'une liste de deux éléments
+	"""
+	if key_size < 1024:
+		return None
+	else:
+		random_gen = Random.new().read
+
+		# Génération de la paire de clé de taille key_size
+		private_key = RSA.generate(key_size, random_gen)
+		public_key  = private_key.publickey()
+
+		# Conversion des clés au format texte (type string)
+		private_key_str = private_key.exportKey().decode()
+		public_key_str  = public_key.exportKey().decode()
+
+		return private_key_str, public_key_str
