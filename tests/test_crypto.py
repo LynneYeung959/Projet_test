2c72004e69b317de5f06068353e236da77f4fdbd
polopaul77 <paul.fourcade1@gmail.com>
2022-01-10 14:47:31 +0100

    :construction: Ajout de la classe TestCrypto pour préparer les tests

---
+++ tests/test_crypto.py
@@ -0,0 +1,13 @@
+import unittest
+import sys
+
+sys.path.append("../client")
+import crypto
+
+class TestCrypto(unittest.TestCase):
+
+	def test_private_key(self):
+		pass
+
+if __name__ == '__main__':
+	unittest.main()
