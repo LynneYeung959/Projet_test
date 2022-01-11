e4efadfb56c63ff462eaa9c8e93e66293d54ef03
polopaul77 <paul.fourcade1@gmail.com>
2022-01-11 12:55:52 +0100

    :construction: Création d'une base de données de test

--- tests/test_database.py
+++ tests/test_database.py
@@ -1,6 +1,10 @@
 import unittest
 import sqlite3
-from server.database import is_username_valid, is_password_valid, is_ip_valid, is_port_valid
+from hashlib import md5
+
+from server.database import is_username_valid, is_password_valid, \
+is_ip_valid, is_port_valid, add_user
+from client.crypto import generate_keys, encrypt_message, decrypt_message
 
 class TestDatabase(unittest.TestCase):
 
@@ -54,13 +58,38 @@ class TestDatabase(unittest.TestCase):
         self.assertFalse(is_port_valid(-10))
         self.assertFalse(is_port_valid(80))
         self.assertFalse(is_port_valid(99999))
-'''
-    def test_addUser(self):
+
+    def test_add_user(self):
         self.assertFalse(database.insert_user_into_db(self.test_database,"","0.0.0.0",80)) # username empty 
         self.assertFalse(database.insert_user_into_db(self.test_database,"log","0.0.0",80)) # bad IP
         self.assertFalse(database.insert_user_into_db(self.test_database,"log","0.0.0.0",70000)) # bad port
         self.assertTrue(database.insert_user_into_db(self.test_database,"log","0.0.0.0",80)) 
         self.assertFalse(database.insert_user_into_db(self.test_database,"log","0.0.0.0",80))  # Cannot add the same user with same address and port
-'''
+
 if __name__ == '__main__':
+
+    # Création d'une table SQL de test
+    conn = sqlite3.connect('test_database.db')
+    cursor = conn.cursor()
+    cursor.execute("DROP TABLE IF EXISTS `Users`")
+    cursor.execute("CREATE TABLE `Users` ( \
+        `username` TEXT UNIQUE NOT NULL, \
+        `password` VARBINARY(32) NOT NULL, \
+        `publickey` TEXT NOT NULL, \
+        `privatekey` TEXT NOT NULL, \
+        `ip` TEXT NOT NULL, \
+        `port` INT UNSIGNED)")
+
+    # Remplir quelques utilisateurs arbitrairement
+    users = [("Gerard", "Pa$$w0rd"), ("Noobmaster69", "xXP@ssw0rdXx")]
+    for username, password in users:
+        md5_pass = md5(password.encode())
+        keys = generate_key(2048)
+        ip = "127.0.0.1"
+        port = 4242
+        cursor.execute("INSERT INTO `users` VALUES(?, ?, ?, ?, ?, ?, ?)", [username, md5_pass.digest(), keys[0], keys[1], ip, port])
+
     unittest.main()
+
+    conn.commit()
+    conn.close()
