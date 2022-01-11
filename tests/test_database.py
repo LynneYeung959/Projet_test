78cd33a2fe236f6f44cf23f7b53159237f9b7508
polopaul77 <paul.fourcade1@gmail.com>
2022-01-11 11:07:07 +0100

    :test_tube: Ajout tu test test_is_username_valid

--- tests/test_database.py
+++ tests/test_database.py
@@ -1,9 +1,24 @@
 import unittest
 import sqlite3
-from server.database import *
+from server.database import is_username_valid, 
 
 class TestDatabase(unittest.TestCase):
 
+    # Test de validation des champs entrés par l'utilisateur :
+
+
+    def test_is_username_valid(self):
+        # Vérification du username
+        self.assertTrue(is_username_valid("abcdefghijklmnopqrstuvwxyz1234567890"))
+        self.assertTrue(is_username_valid("xXyYzZ"))
+        self.assertTrue(is_username_valid("420"))
+
+        self.assertFalse(is_username_valid(""))
+        self.assertFalse(is_username_valid("a"))
+        self.assertFalse(is_username_valid("_Noobmaster69_"))
+        self.assertFalse(is_username_valid("HtmlGoes<br>"))
+        self.assertFalse(is_username_valid("\u00C8re"))  # \u00C8 = È
+'''
 	def test_checkUsername(self):
         self.assertFalse(database.check_username("")) # username empty 
         self.assertTrue(database.check_username("log"))
@@ -24,6 +39,6 @@ class TestDatabase(unittest.TestCase):
         self.assertFalse(database.insert_user_into_db(self.test_database,"log","0.0.0.0",70000)) # bad port
         self.assertTrue(database.insert_user_into_db(self.test_database,"log","0.0.0.0",80)) 
         self.assertFalse(database.insert_user_into_db(self.test_database,"log","0.0.0.0",80))  # Cannot add the same user with same address and port
-
+'''
 if __name__ == '__main__':
     unittest.main()
