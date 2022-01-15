ce544a90df2e47d240d48fb21f14edb03a172997
Modar Nasser <modar1999@gmail.com>
2022-01-15 20:55:29 +0100

    :construction_worker: tests : add __main__ to tests package and update CI accordingly

---
+++ tests/__main__.py
@@ -0,0 +1,63 @@
+"""
+Run `python3 -m tests` from project root to run all test cases as a single TestSuite.
+Add the test modules you want to run to the `test_modules` list below.
+"""
+
+import unittest
+
+test_modules = [
+    'tests.test_crypto',
+    'tests.test_database',
+]
+
+suite = unittest.TestSuite()
+
+for t in test_modules:
+    suite.addTest(unittest.defaultTestLoader.loadTestsFromName(t))
+
+unittest.TextTestRunner().run(suite)
+
+# `print(suite)` will output something like :
+#
+# <unittest.suite.TestSuite tests=[
+#     <unittest.suite.TestSuite tests=[
+#         <unittest.suite.TestSuite tests=[
+#             <tests.test_crypto.TestCrypto testMethod=test_decryption_content>,
+#             <tests.test_crypto.TestCrypto testMethod=test_decryption_key>,
+#             <tests.test_crypto.TestCrypto testMethod=test_decryption_message>,
+#             <tests.test_crypto.TestCrypto testMethod=test_encryption_content>,
+#             <tests.test_crypto.TestCrypto testMethod=test_encryption_key>,
+#             <tests.test_crypto.TestCrypto testMethod=test_encryption_message>,
+#             <tests.test_crypto.TestCrypto testMethod=test_key_creation>,
+#             <tests.test_crypto.TestCrypto testMethod=test_key_size>,
+#             <tests.test_crypto.TestCrypto testMethod=test_key_unique>
+#         ]>
+#     ]>
+# ]>
+#
+# <unittest.suite.TestSuite tests=[
+#     <unittest.suite.TestSuite tests=[
+#         <unittest.suite.TestSuite tests=[
+#             <tests.test_crypto.TestCrypto testMethod=test_decryption_content>,
+#             <tests.test_crypto.TestCrypto testMethod=test_decryption_key>,
+#             <tests.test_crypto.TestCrypto testMethod=test_decryption_message>,
+#             <tests.test_crypto.TestCrypto testMethod=test_encryption_content>,
+#             <tests.test_crypto.TestCrypto testMethod=test_encryption_key>,
+#             <tests.test_crypto.TestCrypto testMethod=test_encryption_message>,
+#             <tests.test_crypto.TestCrypto testMethod=test_key_creation>,
+#             <tests.test_crypto.TestCrypto testMethod=test_key_size>,
+#             <tests.test_crypto.TestCrypto testMethod=test_key_unique>
+#         ]>
+#     ]>,
+#     <unittest.suite.TestSuite tests=[
+#         <unittest.suite.TestSuite tests=[
+#             <tests.test_database.TestDatabase testMethod=test_is_ip_valid>,
+#             <tests.test_database.TestDatabase testMethod=test_is_password_valid>,
+#             <tests.test_database.TestDatabase testMethod=test_is_port_valid>,
+#             <tests.test_database.TestDatabase testMethod=test_is_user_registered>,
+#             <tests.test_database.TestDatabase testMethod=test_is_username_valid>,
+#             <tests.test_database.TestDatabase testMethod=test_user_create>,
+#             <tests.test_database.TestDatabase testMethod=test_user_login>
+#         ]>
+#     ]>
+# ]>
