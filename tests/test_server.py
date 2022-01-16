b7a62ff7612d39cddae2d8588bd2c6b34be366ec
Modar Nasser <modar1999@gmail.com>
2022-01-16 19:46:34 +0100

    :test_tube: tests : add failing server tests

--- tests/test_server.py
+++ tests/test_server.py
@@ -23,6 +23,12 @@ class TestServer(unittest.TestCase):
         cls.server_subprocess.start()
         time.sleep(3)
 
+        @server.database.connect(cls.database_name)
+        def create_test_users():
+            server.database.DB.user_create("paul", "1P@ssword", "127.0.0.13", 1999)
+
+        create_test_users()
+
     @classmethod
     def tearDownClass(cls):
         cls.server_subprocess.kill()
@@ -54,18 +60,33 @@ class TestServer(unittest.TestCase):
         response = requests.get(self.server_url + "/users/paul/ip")
         self.assertEqual(response.status_code, 200)
 
-        # correct request (should fail later)
+        # correct request, but nonexistent user
         response = requests.get(self.server_url + "/users/12$/ip")
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, 404)
 
         # incorrect request (URL shall contain username)
         response = requests.get(self.server_url + "/users/ip")
-        self.assertEqual(response.status_code, 404)
+        self.assertEqual(response.status_code, 405)
 
     def test_delete_user(self):
         # correct request
-        response = requests.delete(self.server_url + "/users/paul")
+        payload = {'password': '1P@ssword'}
+        response = requests.delete(self.server_url + "/users/paul", json=payload)
         self.assertEqual(response.status_code, 200)
+
+        # incomplete request (missing data)
+        response = requests.delete(self.server_url + "/users/paul")
+        self.assertEqual(response.status_code, 422)
+
+        # incorrect parameters
+        payload = {'pass': '1P@ssword'}
+        response = requests.delete(self.server_url + "/users/paul", json=payload)
+        self.assertEqual(response.status_code, 422)
+
+        # correct request, but incorrect credentials
+        payload = {'password': 'P@ssword'}
+        response = requests.delete(self.server_url + "/users/paul", json=payload)
+        self.assertEqual(response.status_code, 403)
 
         # unsupported request
         response = requests.get(self.server_url + "/users/paul")
