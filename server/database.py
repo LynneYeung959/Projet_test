7d8606dff5afbb80df496e437e850d0835c1beaa
Modar Nasser <modar1999@gmail.com>
2022-01-16 18:50:18 +0100

    :white_check_mark: server.database : implement DataBase methods and pass tests

--- server/database.py
+++ server/database.py
@@ -36,39 +36,38 @@ class Database:
     def drop_tables(self):
         self.conn.cursor().execute("DROP TABLE IF EXISTS `Users`")
 
+    def user_exists(self, username: str) -> bool:
+        cursor = self.conn.cursor()
+        cursor.execute("SELECT username FROM `Users` WHERE username=?", [username])
+        return len(cursor.fetchall()) > 0
+
+    def user_create(self, username: str, password: str, ip_address: str, port: int) -> bool:
+        """ Create a new user with the given arguments and insert it in the database
+            Returns True if successful, False otherwise
+        """
+        # Test de validité des arguments (optionnel, doivent être vérifiés avant l'appel de cette fonction
+        if is_username_valid(username) and is_password_valid(password) and is_ip_valid(ip_address) and is_port_valid(port):
+            if not self.user_exists(username):
+                # Création du nouvel utilisateur
+                md5_pass = md5(password.encode())
+                keys = KeyPair.generate(2048)
+                cursor = self.conn.cursor()
+                cursor.execute("INSERT INTO `Users` VALUES(?, ?, ?, ?, ?, ?)",
+                               [username, md5_pass.digest(), keys.public, keys.private, ip_address, port])
+                return True
+        return False
+
+    def user_login(self, username: str, password: str):
+        """ Try to log in user using username and password
+            Returns True if successful, False otherwise
+        """
+        md5_pass = md5(password.encode())
+        cursor = self.conn.cursor()
+        cursor.execute("SELECT username FROM `Users` WHERE username=? AND password=?", [username, md5_pass.digest()])
+        return len(cursor.fetchall()) > 0
+
 
 DB:  Optional[Database] = None
-
-
-def is_user_registered(cursor: sqlite3.Cursor, username: str) -> bool:
-    """ Check if username already exists in database """
-    cursor.execute("SELECT username FROM `Users` WHERE username=?", [username])
-    return len(cursor.fetchall()) > 0  # La liste est vide si username n'est pas trouvé
-
-
-def user_login(cursor: sqlite3.Cursor, username: str, password: str) -> bool:
-    """ Try to log in user using username and password
-        Returns True if successful, False otherwise
-    """
-    md5_pass = md5(password.encode())
-    cursor.execute("SELECT username FROM `Users` WHERE username=? AND password=?", [username, md5_pass.digest()])
-    return len(cursor.fetchall()) > 0
-
-
-def user_create(cursor: sqlite3.Cursor, username: str, password: str, ip_address: str, port: int) -> bool:
-    """ Create a new user with the given arguments and insert it in the database
-        Returns True if successful, False otherwise
-    """
-    # Test de validité des arguments (optionnel, doivent être vérifiés avant l'appel de cette fonction
-    if is_username_valid(username) and is_password_valid(password) and is_ip_valid(ip_address) and is_port_valid(port):
-        if not is_user_registered(cursor, username):
-            # Création du nouvel utilisateur
-            md5_pass = md5(password.encode())
-            keys = KeyPair.generate(2048)
-            cursor.execute("INSERT INTO `Users` VALUES(?, ?, ?, ?, ?, ?)",
-                           [username, md5_pass.digest(), keys.public, keys.private, ip_address, port])
-            return True
-    return False
 
 
 def connect(db_name: str) -> Callable:
