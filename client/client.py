0e31a42b4ece72f9d7b213739ba6443fd7ce1e18
Modar Nasser <modar1999@gmail.com>
2022-01-22 21:39:24 +0100

    :sparkles: client : client specify server addr and port in command line + user login implemented

--- client/client.py
+++ client/client.py
@@ -1,14 +1,24 @@
+import argparse
+import sys
+
 import requests
 
 
 if __name__ == '__main__':
-    print("Please enter server address : ")
-    server_address = input("> ")
+    parser = argparse.ArgumentParser(description='Launch client connected at specified address and port')
+    parser.add_argument('--addr', default='localhost', type=str, help='specify server address (default : localhost)')
+    parser.add_argument('--port', default=80, type=int, help='specify server port (default : 80)')
+    args = parser.parse_args()
 
-    print("Please enter server port : ")
-    server_port = input("> ")
+    server_url = f"http://{args.addr}:{args.port}"
 
-    server_url = f"http://{server_address}:{server_port}"
+    try:
+        response = requests.get(server_url + '/isalive')
+        if response.status_code != 200:
+            print(f"Server at {server_url} is down !")
+    except requests.exceptions.ConnectionError:
+        print(f"Failed to connect to {server_url} !")
+        sys.exit(1)
 
     print("Your username (at least 3 characters, only letters or digits) : ")
     username = input("> ")
@@ -16,6 +26,7 @@ if __name__ == '__main__':
     # check if user exists by getting its IP from server
     response = requests.get(server_url + '/users/' + username + '/ip')
 
+    # connect and login user
     if response.status_code == 404:
         print(f"Welcome {username} !\n"
               f"Create your account by entering a new password\n"
@@ -34,5 +45,18 @@ if __name__ == '__main__':
 
     else:
         print(f"Welcome back {username} !\n"
-              f"Please enter your password to log in")
+              f"Please enter your password to log in :")
         password = input("> ")
+        data = {'username': username, 'password': password}
+        response = requests.post(server_url + '/sessions', json=data)
+        while response.status_code == 400:
+            print("Bad credentials, please try again :")
+            username = input("Your username : ")
+            password = input("Your password : ")
+            data = {'username': username, 'password': password}
+            response = requests.post(server_url + '/sessions', json=data)
+
+        print("Logged in successfully !")
+
+    # user logged in
+    ...
