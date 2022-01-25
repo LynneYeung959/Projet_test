a597c3426f1ec805e3c4e6b3429da0ebe848cb96
Modar Nasser <modar1999@gmail.com>
2022-01-25 10:25:35 +0100

    :bug: client :! add missing file !

---
+++ client/msg_server.py
@@ -0,0 +1,19 @@
+from flask import Flask, request
+
+
+def run_message_server(port: int):
+    app = Flask(__name__)
+
+    import logging
+    log = logging.getLogger('werkzeug')
+    log.setLevel(logging.ERROR)
+
+    @app.route('/msg', methods=['POST'])
+    def on_msg():
+        msg = str(request.data).encode()
+        print("> received :", msg)
+        return "", 200
+
+    app.run(port=port)
+
+
