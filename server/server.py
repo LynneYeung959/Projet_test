c80fe3b960bafe72db511879c6e922e9f8ea8bda
TheMomonkey <36695513+TheMomonkey@users.noreply.github.com>
2022-01-25 09:37:16 +0100

    :rotating_light: tabs to spaces (again)

--- server/server.py
+++ server/server.py
@@ -1,9 +1,9 @@
 """server
 Usage:
-	server.py --port=<int>
+    server.py --port=<int>
 Options:
-	-h --help     Show this screen.
-	--port=<int>  port used
+    -h --help     Show this screen.
+    --port=<int>  port used
 """
 import logging
 from docopt import docopt
@@ -19,38 +19,38 @@ APP = Flask(__name__)
 # Check server state
 @APP.route('/isalive', methods=['GET'])
 def is_alive():
-	return Response(status=200)
+    return Response(status=200)
 
 
 
 # Add user
 @APP.route('/users', methods=['POST'])
 def add_user():
-	data = request.form
-	result = db.insert_user_into_db(data['username'], data['IP'], int(data['port']))
-	if not result:
-		logging.warning("Failed to register user")
-		return Response(status=404)
-	return Response(status=200)
+    data = request.form
+    result = db.insert_user_into_db(data['username'], data['IP'], int(data['port']))
+    if not result:
+        logging.warning("Failed to register user")
+        return Response(status=404)
+    return Response(status=200)
 
 # Get IP with username
 @APP.route('/users/<string:username>/ip', methods=['GET'])
 def get_IP(username):
-	pass
+    pass
 
 # Delete user with username
 @APP.route('/users/<string:username>', methods=['DELETE'])
 def delete_user(username):
-	pass
+    pass
 
 
 
 if __name__ == '__main__':
-	db.init_db()
-	db.dump_db()
-	APP.run()
-	# ARGS = docopt(__doc__)
-	# if ARGS['--port']:
-	# 	APP.run(host='0.0.0.0', port=ARGS['--port'])
-	# else:
-	# 	logging.error("Wrong command line arguments")
+    db.init_db()
+    db.dump_db()
+    APP.run()
+    # ARGS = docopt(__doc__)
+    # if ARGS['--port']:
+    # 	APP.run(host='0.0.0.0', port=ARGS['--port'])
+    # else:
+    # 	logging.error("Wrong command line arguments")
--- tests/test_server.py
+++ tests/test_server.py
@@ -11,75 +11,75 @@ import server
 
 class TestServer(unittest.TestCase):
 
-	server_subprocess = None
+    server_subprocess = None
 
-	test_port = "5000"
-	server_address = "127.0.0.1"
-	server_url = "http://" + server_address + ":" + test_port
+    test_port = "5000"
+    server_address = "127.0.0.1"
+    server_url = "http://" + server_address + ":" + test_port
 
-	def set_up(self):
-		cmd = "python3 server.py"
-		args = shlex.split(cmd)
-		self.server_subprocess  = subprocess.Popen(args) # launch command as a subprocess
-		time.sleep(3)
+    def set_up(self):
+        cmd = "python3 server.py"
+        args = shlex.split(cmd)
+        self.server_subprocess  = subprocess.Popen(args) # launch command as a subprocess
+        time.sleep(3)
 
-	def test_launch_server(self):
-		response = requests.get(self.server_url+"/isalive")
-		self.assertEqual(response.status_code, 200)
+    def test_launch_server(self):
+        response = requests.get(self.server_url+"/isalive")
+        self.assertEqual(response.status_code, 200)
 
-	def test_add_user_request(self):
-		# test correct user insertion
-		payload = {'username': 'alice', 'IP': '0.0.0.14', 'port': '1236'}
-		response = requests.post(self.server_url+"/users", data=payload)
-		self.assertEqual(response.status_code, 200)
+    def test_add_user_request(self):
+        # test correct user insertion
+        payload = {'username': 'alice', 'IP': '0.0.0.14', 'port': '1236'}
+        response = requests.post(self.server_url+"/users", data=payload)
+        self.assertEqual(response.status_code, 200)
 
-		# test multiple users insertion with same username (should fail)
-		payload = {'username': 'albert', 'IP': '0.0.0.0', 'port': '1234'}
-		response = requests.post(self.server_url+"/users", data=payload)
-		payload = {'username': 'albert', 'IP': '0.0.0.1', 'port': '1235'}
-		response = requests.post(self.server_url+"/users", data=payload)
-		self.assertEqual(response.status_code, 404)
+        # test multiple users insertion with same username (should fail)
+        payload = {'username': 'albert', 'IP': '0.0.0.0', 'port': '1234'}
+        response = requests.post(self.server_url+"/users", data=payload)
+        payload = {'username': 'albert', 'IP': '0.0.0.1', 'port': '1235'}
+        response = requests.post(self.server_url+"/users", data=payload)
+        self.assertEqual(response.status_code, 404)
 
-		# test unsupported request type
-		response = requests.get(self.server_url+"/users")
-		self.assertEqual(response.status_code, 405)
+        # test unsupported request type
+        response = requests.get(self.server_url+"/users")
+        self.assertEqual(response.status_code, 405)
 
-		# wrong URL
-		response = requests.get(self.server_url+"/user")
-		self.assertEqual(response.status_code, 404)
+        # wrong URL
+        response = requests.get(self.server_url+"/user")
+        self.assertEqual(response.status_code, 404)
 
-	def test_getIP(self):
-		# correct request
-		response = requests.get(self.server_url+"/users/paul/ip")
-		self.assertEqual(response.status_code, 200)
+    def test_getIP(self):
+        # correct request
+        response = requests.get(self.server_url+"/users/paul/ip")
+        self.assertEqual(response.status_code, 200)
 
-		# correct request (should fail later)
-		response = requests.get(self.server_url+"/users/12$/ip")
-		self.assertEqual(response.status_code, 200)
+        # correct request (should fail later)
+        response = requests.get(self.server_url+"/users/12$/ip")
+        self.assertEqual(response.status_code, 200)
 
-		# incorrect request (URL shall contain username)
-		response = requests.get(self.server_url+"/users/ip")
-		self.assertEqual(response.status_code, 404)
+        # incorrect request (URL shall contain username)
+        response = requests.get(self.server_url+"/users/ip")
+        self.assertEqual(response.status_code, 404)
 
-	def test_delete_user(self):
-		# correct request
-		response = requests.delete(self.server_url+"/users/paul")
-		self.assertEqual(response.status_code, 200)
+    def test_delete_user(self):
+        # correct request
+        response = requests.delete(self.server_url+"/users/paul")
+        self.assertEqual(response.status_code, 200)
 
-		# unsupported request
-		response = requests.get(self.server_url+"/users/paul")
-		self.assertEqual(response.status_code, 405)
+        # unsupported request
+        response = requests.get(self.server_url+"/users/paul")
+        self.assertEqual(response.status_code, 405)
 
-		# try to delete all users (not allowed)
-		response = requests.delete(self.server_url+"/users")
-		self.assertEqual(response.status_code, 405)
+        # try to delete all users (not allowed)
+        response = requests.delete(self.server_url+"/users")
+        self.assertEqual(response.status_code, 405)
 
 
-	def tear_down(self):
-		print("killing subprocess server")
-		self.server_subprocess.kill()
-		self.server_subprocess.wait()
+    def tear_down(self):
+        print("killing subprocess server")
+        self.server_subprocess.kill()
+        self.server_subprocess.wait()
 
 
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
